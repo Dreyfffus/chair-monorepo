b3dee0b754fd22ba95b8f273e86071598ccefa62
Zica Stefan Alexandru <stef.zica@gmail.com>
2026-05-21 18:24:44 +0200

    Delete backend directory

--- backend/migrations/20260302125659_create_users_table.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    display_name TEXT,
-    password_hash TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
--- backend/migrations/20260302132425_create_sessions_table.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    session_token_hash TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    last_seen_at TIMESTAMP DEFAULT NOW(),
-    expires_at TIMESTAMP NOT NULL
-);
